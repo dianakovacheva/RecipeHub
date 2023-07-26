8a7ab09cb363374ec06cc0341c9f798d3e4061ca
Diana Kovacheva <dmarinova@outlook.com>
2023-07-26 22:18:26 +0200

    Adds auth to the user related routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,36 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
-import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { HomeComponent } from "./home/home.component";
+import { RecipeDetailsComponent } from "./recipe-details/recipe-details.component";
 
 const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: '/',
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/home",
   },
   {
-    path: '',
+    path: "home",
     component: HomeComponent,
   },
   {
-    path: 'recipes',
+    path: "auth",
+    loadChildren: () =>
+      import("./user/user.module").then((model) => model.UserModule),
+  },
+  {
+    path: "recipes",
     component: RecipeListComponent,
   },
   {
-    path: 'recipe-details',
+    path: "recipe-details/:recipeId",
     component: RecipeDetailsComponent,
   },
   {
-    path: '**',
+    path: "**",
     component: PageNotFoundComponent,
   }, // Wildcard route for a 404 page
 ];
