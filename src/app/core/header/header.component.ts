b647f9157904c04848dfdb23271dd422497d4e73
Diana Kovacheva <dmarinova@outlook.com>
2023-07-27 21:10:26 +0200

    Adds logic to check if user is logged in

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatTabsModule } from '@angular/material/tabs';
-import { Router, RouterModule } from '@angular/router';
+import { CommonModule, NgIf } from "@angular/common";
+import { Component } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatTabsModule } from "@angular/material/tabs";
+import { Router, RouterModule } from "@angular/router";
+import { UserService } from "src/app/user/user.service";
 
 /**
  * @title Basic toolbar
  */
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
   standalone: true,
   imports: [
     MatToolbarModule,
@@ -22,9 +24,28 @@ import { Router, RouterModule } from '@angular/router';
     MatMenuModule,
     MatTabsModule,
     RouterModule,
+    CommonModule,
+    NgIf,
   ],
 })
-export class HeaderComponent implements OnInit {
-  constructor(private router: Router) {}
-  ngOnInit() {}
+export class HeaderComponent {
+  constructor(private userService: UserService, private router: Router) {}
+
+
+  get userFullName(): string {
+    const userFistName = this.userService.user?.firstName;
+    const userLastName = this.userService.user?.lastName;
+    return `Welcome ${userFistName} ${userLastName}` || "";
+  }
+
+  logout(): void {
+    this.userService.logout().subscribe({
+      next: () => {
+        this.router.navigate(["/auth/login"]);
+      },
+      error: () => {
+        this.router.navigate(["/auth/login"]);
+      },
+    });
+  }
 }
