1f2c09477ecf54487eff46f0240e2fea152daa6f
Diana Kovacheva <dmarinova@outlook.com>
2023-07-29 12:19:30 +0200

    Adds router model to the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
 
-import { NgFor } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatBadgeModule } from '@angular/material/badge';
+import { NgFor } from "@angular/common";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatBadgeModule } from "@angular/material/badge";
 
 /**
  * @title Dynamic grid-list
  */
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
   standalone: true,
-  imports: [NgFor, MatCardModule, MatButtonModule, MatBadgeModule],
+  imports: [
+    NgFor,
+    MatCardModule,
+    MatButtonModule,
+    MatBadgeModule,
+    RouterModule,
+  ],
 })
 export class HomeComponent implements OnInit {
   constructor(private router: Router) {}
