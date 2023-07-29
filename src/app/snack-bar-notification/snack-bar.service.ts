143450ecbb782227a8b965c119fde62bcf886c57
Diana Kovacheva <dmarinova@outlook.com>
2023-07-29 17:53:20 +0200

    Creates different snack bar notifications

--- src/app/snack-bar-notification/snack-bar.service.ts
+++ src/app/snack-bar-notification/snack-bar.service.ts
@@ -6,3 +6,37 @@
   // Specify the position of the snack bar
   horizontalPosition: MatSnackBarHorizontalPosition = "end";
   verticalPosition: MatSnackBarVerticalPosition = "top";
+  // Different snack bar notifications
+  greetUser() {
+    this.notifyInfo(`Welcome, ${this.userFullName}`);
+  }
+
+  goodbyeUser() {
+    this.notifyInfo("Logged out successfully");
+  }
+
+  notifySuccess(message: string) {
+    this.snackBar.open(message, undefined, {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      panelClass: ["snackbar-success"],
+      duration: 3000,
+    });
+  }
+
+  notifyError(message: string) {
+    this.snackBar.open(message, undefined, {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      panelClass: ["snackbar-error"],
+      duration: 3000,
+    });
+  }
+
+  notifyInfo(message: string) {
+    this.snackBar.open(message, undefined, {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: 3000,
+    });
+  }
