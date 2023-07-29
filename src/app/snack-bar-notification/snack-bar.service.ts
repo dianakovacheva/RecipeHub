a098b097bdf3f3c10d9fc22be36384e60da4a163
Diana Kovacheva <dmarinova@outlook.com>
2023-07-29 17:52:38 +0200

    Specify the position of the snack bar

--- src/app/snack-bar-notification/snack-bar.service.ts
+++ src/app/snack-bar-notification/snack-bar.service.ts
@@ -3,3 +3,6 @@
     const userFistName = this.userService.user?.firstName;
     const userLastName = this.userService.user?.lastName;
     return `${userFistName} ${userLastName}` || "";
+  // Specify the position of the snack bar
+  horizontalPosition: MatSnackBarHorizontalPosition = "end";
+  verticalPosition: MatSnackBarVerticalPosition = "top";
