45dc805a9f4fd9804790de3c3866ae1aa9d2684c
Diana Kovacheva <dmarinova@outlook.com>
2023-07-29 16:24:38 +0200

    Adds the spoonacularAPI URL

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
   backendURL: "http://localhost:3000/api",
+  spoonacularAPI_URL: "https://api.spoonacular.com/recipes/",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
   backendURL: "http://localhost:3000/api", // domain -> will not be localhost in prod
+  spoonacularAPI_URL: "https://api.spoonacular.com/recipes/",
 };
