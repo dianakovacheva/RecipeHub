65ece38ab0868358fcd73b0651c1a48a5284665d
Diana Kovacheva <dmarinova@outlook.com>
2023-07-29 17:53:44 +0200

    Adds snack bar to the app

--- src/app/snack-bar-notification/snack-bar.service.ts
+++ src/app/snack-bar-notification/snack-bar.service.ts
@@ -1,11 +1,34 @@
+import { Injectable } from "@angular/core";
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from "@angular/material/snack-bar";
+import { Subject } from "rxjs";
+import { UserService } from "src/app/user/user.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class SnackBarService {
+  public notification$: Subject<string> = new Subject();
+
+  constructor(
+    private userService: UserService,
+    private snackBar: MatSnackBar
+  ) {}
+
   // Get user's first and last name
   get userFullName(): string {
     const userFistName = this.userService.user?.firstName;
     const userLastName = this.userService.user?.lastName;
     return `${userFistName} ${userLastName}` || "";
+  }
+
   // Specify the position of the snack bar
   horizontalPosition: MatSnackBarHorizontalPosition = "end";
   verticalPosition: MatSnackBarVerticalPosition = "top";
+
   // Different snack bar notifications
   greetUser() {
     this.notifyInfo(`Welcome, ${this.userFullName}`);
@@ -40,3 +63,4 @@
       duration: 3000,
     });
   }
+}
