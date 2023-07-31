201f006c8b5dbe04bf306e1febfa2920af38ef54
Diana Kovacheva <dmarinova@outlook.com>
2023-07-31 21:14:20 +0200

    Creates recipe model

---
+++ src/app/models/Recipe.ts
@@ -0,0 +1,20 @@
+import { UserId } from "./UserId";
+
+export interface User {
+  recipeApiId: string;
+  _id: string;
+  title: string;
+  author: UserId;
+  preparationMinutes: number;
+  cookingMinutes: number;
+  servings: number;
+  pricePerServing: number;
+  imageURL: string;
+  summary: string;
+  dishTypes: string[];
+  extendedIngredients: any;
+  analyzedInstructions: any;
+  commentsList: string[];
+  saves: string[];
+  createdAt: string;
+}
