08478b4b5a8dd7d27424950382a57355cfffb05d
Diana Kovacheva <dmarinova@outlook.com>
2023-08-03 10:05:14 +0200

    Shows different button based on if user is logged in or guest

--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -1,41 +1,58 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { NgFor } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { NgIf, NgFor } from "@angular/common";
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
+import { CommonModule } from "@angular/common";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
 
-/**
- * @title Dynamic grid-list
- */
+import { Recipe } from "../models/Recipe";
+import { RecipeService } from "../recipe/recipe.service";
+import { UserService } from "../user/user.service";
 
 @Component({
-  selector: 'app-recipe-details',
-  templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.css'],
+  selector: "app-recipe-details",
+  templateUrl: "./recipe-details.component.html",
+  styleUrls: ["./recipe-details.component.css"],
   standalone: true,
   imports: [
     MatIconModule,
     MatListModule,
     MatCardModule,
     MatGridListModule,
-    NgFor,
     MatButtonModule,
+    CommonModule,
+    NgIf,
+    NgFor,
+    MatDividerModule,
   ],
 })
-export class RecipeDetailsComponent {
-  tiles: Tile[] = [
-    { text: 'One', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Two', cols: 1, rows: 1, color: 'lightgreen' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Four', cols: 1, rows: 1, color: 'lightgreen' },
-  ];
+export class RecipeDetailsComponent implements OnInit {
+  recipe: Recipe | undefined;
+
+  constructor(
+    private recipeService: RecipeService,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLoggedIn;
+  }
+
+  ngOnInit(): void {
+    this.getRecipeById();
+  }
+
+  getRecipeById(): void {
+    const recipeId = this.activatedRoute.snapshot.params["recipeId"];
+
+    this.recipeService.getRecipeById(recipeId).subscribe((foundRecipe) => {
+      this.recipe = foundRecipe;
+    });
+  }
 }
