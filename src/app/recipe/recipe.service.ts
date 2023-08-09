1c195634522b0f30fd9105a7ddb64697be6eef67
Diana Kovacheva <dmarinova@outlook.com>
2023-08-09 22:03:40 +0200

    Adds method saveRecipe

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -7,24 +7,20 @@ import { BehaviorSubject, catchError, tap, throwError } from "rxjs";
 
 const backendURL = environment.backendURL;
 
-// Define the structure of the type cooking step data returned from the API
 type cookingStep = {
   number: number;
   step: string;
 };
 
-// Define the structure of the type analyzedInstructions data returned from the API
 type analyzedInstructions = {
   steps: cookingStep[];
 };
 
-// Define the structure of the type measure data returned from the API
 type Measure = {
   amount: number;
   unitShort: string;
 };
 
-// Define the structure of the type extendedIngredient data returned from the API
 type ExtendedIngredient = {
   name: string;
   measures: {
@@ -47,7 +43,6 @@ export class RecipeService {
 
     const returnObject: ExtendedIngredient[] = [];
 
-    // Loop through the ingredients array and split each ingredient into its amount, unit and name by ">"
     for (const ingredient of ingredientsArr) {
       const [amount, unitShort, name] = ingredient.split(" > ");
       const measure: Measure = {
@@ -64,17 +59,17 @@ export class RecipeService {
     return returnObject;
   }
 
-  // Convert Steps to Array of Objects
   convertStepstoArray(steps: string) {
     let stepsArr = steps.split("\n");
     stepsArr = stepsArr.filter((step) => step).map((step) => step.trim());
-    // Create an array of objects with the structure of the type analyzedInstructions
+
+    // Create an array of objects with the steps
     const returnObject: analyzedInstructions[] = [{ steps: [] }];
 
     for (const [i, step] of stepsArr.entries()) {
       returnObject[0].steps.push({ number: i + 1, step: step });
     }
-    // Return the array of objects as a string to fit the API's requirenments
+
     return JSON.stringify(returnObject);
   }
 
@@ -122,10 +117,6 @@ export class RecipeService {
     https: return this.http.get<Recipe[]>(`${backendURL}/recipes`);
   }
 
-  // .subscribe((recipe: Recipe) => {
-  //   this.userIsOwner = recipe?.author?._id === this.userService.user?._id;
-  // });
-
   // Get Recipe by Id
   getRecipeById(recipeId: string) {
     https: return this.http
@@ -143,7 +134,7 @@ export class RecipeService {
       );
   }
 
-  // change UserIsOwner
+  // Change UserIsOwner
   updateUserIsOwner(recipeId: string): void {
     let isOwner = false;
     this.userService.getProfile();
@@ -155,7 +146,6 @@ export class RecipeService {
     this.userIsOwner$$.next(isOwner);
   }
 
-  // Convert the recipe steps to the correct format required by the API
   convertRecipeSteps(steps: []) {
     const convertedSteps: string = steps
       .map((step: { step: string }) => step.step)
@@ -163,7 +153,6 @@ export class RecipeService {
     return convertedSteps;
   }
 
-  // Convert the recipe ingredients to the correct format required by the API
   convertRecipeIngredients(ingredients: []) {
     const convertedIngredients: string = ingredients
       .map(
@@ -210,6 +199,20 @@ export class RecipeService {
           extendedIngredients: convertedIngredients,
           analyzedInstructions: convertedSteps,
         },
+        {
+          withCredentials: true,
+        }
+      )
+      .pipe(tap(() => this.userService.getProfile()));
+  }
+
+  // Save Recipe
+  saveRecipe(recipeId: string) {
+    console.log(this.userService.user);
+    https: return this.http
+      .put<Recipe>(
+        `${backendURL}/recipes/details/${recipeId}/save`,
+        {},
         {
           withCredentials: true,
         }
