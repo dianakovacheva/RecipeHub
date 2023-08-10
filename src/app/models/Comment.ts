a81ce0fa7ca423d07593a2b6802e62719f87b10d
Diana Kovacheva <dmarinova@outlook.com>
2023-08-10 23:13:00 +0200

    Adjusts the comment interface

--- src/app/models/Comment.ts
+++ src/app/models/Comment.ts
@@ -2,7 +2,8 @@ import { UserId } from "./UserId";
 import { Recipe } from "./Recipe";
 
 export interface Comment {
-  text: string;
-  commentOwner: UserId;
+  comment: string;
+  commentAuthor: UserId;
   commentedRecipe: Recipe;
+  created_at: string;
 }
