1b8dcb9b55d96167cfc7525587be5912e47fa32e
Diana Kovacheva <dmarinova@outlook.com>
2023-08-11 18:04:12 +0200

    Adds function handle delete comment

--- src/app/recipe/comment-card/comment-card.component.ts
+++ src/app/recipe/comment-card/comment-card.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { MatChipsModule } from "@angular/material/chips";
 import { CommonModule } from "@angular/common";
 import { MatCardModule } from "@angular/material/card";
-import { MatChipsModule } from "@angular/material/chips";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+
 import { RecipeDetailsComponent } from "../recipe-details/recipe-details.component";
 import { Comment } from "src/app/models/Comment";
 import * as moment from "moment";
+
+import { RecipeService } from "../recipe.service";
+import { SnackBarService } from "src/app/shared/snack-bar-notification/snack-bar.service";
+import { Recipe } from "src/app/models/Recipe";
+import { UserService } from "src/app/user/user.service";
 
 @Component({
   selector: "app-comment-card",
@@ -14,6 +22,8 @@ import * as moment from "moment";
     MatCardModule,
     RecipeDetailsComponent,
     MatChipsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: "./comment-card.component.html",
   styleUrls: ["./comment-card.component.css"],
@@ -22,13 +32,39 @@ export class CommentCardComponent {
   @Input() comment: Comment | undefined;
   @Input() ownComment: boolean | undefined;
   @Input() commentByOwner: boolean | undefined;
+  @Input() recipeId: string | undefined;
   commentSince: string | undefined;
 
-  constructor() {}
+  constructor(
+    private recipeService: RecipeService,
+    private userService: UserService,
+    private snackBar: SnackBarService,
+    private recipeDetailsComponent: RecipeDetailsComponent
+  ) {}
+
   getCommentSince(createdAt: string | undefined): string {
     if (!createdAt) {
       return "";
     }
     return moment(createdAt).fromNow();
+  }
+
+  handleDeleteRecipeComment() {
+    if (!this.comment) {
+      return;
+    }
+    this.recipeService
+      .deleteRecipeComment(this.comment._id, this.recipeId!)
+      .subscribe({
+        next: () => {
+          this.recipeDetailsComponent.getRecipeById();
+          this.recipeDetailsComponent.getRecipeComments();
+          this.userService.getProfile();
+          this.snackBar.notifySuccess("Comment deleted successfully!");
+        },
+        error: (error) => {
+          this.snackBar.notifyError(error.error.message);
+        },
+      });
   }
 }
