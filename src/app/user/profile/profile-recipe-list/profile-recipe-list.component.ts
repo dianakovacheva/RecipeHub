3882edeb8aeb64581656a9a65ba79e6bd7473af5
Diana Kovacheva <dmarinova@outlook.com>
2023-08-11 18:14:55 +0200

    Adds comments

--- src/app/user/profile/profile-recipe-list/profile-recipe-list.component.ts
+++ src/app/user/profile/profile-recipe-list/profile-recipe-list.component.ts
@@ -6,12 +6,14 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 import { UserService } from "../../user.service";
 import { Recipe } from "src/app/models/Recipe";
+import { Comment } from "src/app/models/Comment";
 import { DeleteRecipeComponent } from "src/app/recipe/delete-recipe/delete-recipe.component";
 import { MatButtonModule } from "@angular/material/button";
 import { NgIf, NgFor } from "@angular/common";
 import { ProfileComponent } from "../profile.component";
 import { RecipeService } from "src/app/recipe/recipe.service";
 import { SnackBarService } from "src/app/shared/snack-bar-notification/snack-bar.service";
+// import { CommentRecipeComponent } from "src/app/recipe/comment-recipe/comment-recipe.component";
 
 @Component({
   selector: "app-profile-recipe-list",
@@ -31,16 +33,22 @@ import { SnackBarService } from "src/app/shared/snack-bar-notification/snack-bar
 })
 export class ProfileRecipeListComponent {
   @Input() recipesArray: Recipe[] | undefined;
-  @Input() listType: "userRecipes" | "userSavedRecipes" | undefined;
+  @Input() commentsArray: Comment[] | undefined;
+  @Input() listType:
+    | "userRecipes"
+    | "userSavedRecipes"
+    // | "userCommentsList"
+    | undefined;
 
   constructor(
-    private userService: UserService,
     private recipeService: RecipeService,
     private dialog: MatDialog,
     private profileComponent: ProfileComponent,
+    // private commentComponent: CommentRecipeComponent,
     private snackBar: SnackBarService
   ) {}
 
+  // Delete Recipe Dialog
   openDeleteDialog(recipe: Recipe, $event: any): void {
     $event.stopPropagation();
     this.dialog
@@ -53,6 +61,7 @@ export class ProfileRecipeListComponent {
       });
   }
 
+  // Remove Saved Recipe
   handelRemoveSavedRecipe(recipeId: string, $event: any): void {
     $event.stopPropagation();
     this.recipeService.removeSavedRecipe(recipeId).subscribe(() => {
@@ -60,4 +69,20 @@ export class ProfileRecipeListComponent {
       this.snackBar.notifySuccess("Recipe removed successfully!");
     });
   }
+
+  // Comment Recipe Dialog
+  // openCommentDialog(comment: Comment, $event: any): void {
+  //   $event.stopPropagation();
+  //   this.dialog
+  //     .open(CommentRecipeComponent, {
+  //       data: {
+  //         comment: comment,
+  //         redirectToRecipes: false,
+  //       },
+  //     })
+  //     .afterClosed()
+  //     .subscribe(() => {
+  //       this.profileComponent.getUserComments();
+  //     });
+  // }
 }
