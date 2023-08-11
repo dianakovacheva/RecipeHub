8ac548af46d8fc62a2388ce56c83ed926774d276
Diana Kovacheva <dmarinova@outlook.com>
2023-08-11 10:49:31 +0200

    Shows comment card with user's info and comment

--- src/app/recipe/comment-card/comment-card.component.ts
+++ src/app/recipe/comment-card/comment-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatCardModule } from "@angular/material/card";
-import { Recipe } from "src/app/models/Recipe";
+import { MatChipsModule } from "@angular/material/chips";
 import { RecipeDetailsComponent } from "../recipe-details/recipe-details.component";
 import { Comment } from "src/app/models/Comment";
 import * as moment from "moment";
@@ -9,12 +9,18 @@ import * as moment from "moment";
 @Component({
   selector: "app-comment-card",
   standalone: true,
-  imports: [CommonModule, MatCardModule, RecipeDetailsComponent],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    RecipeDetailsComponent,
+    MatChipsModule,
+  ],
   templateUrl: "./comment-card.component.html",
   styleUrls: ["./comment-card.component.css"],
 })
 export class CommentCardComponent {
   @Input() comment: Comment | undefined;
+  @Input() ownComment: boolean | undefined;
   commentSince: string | undefined;
 
   constructor() {}
