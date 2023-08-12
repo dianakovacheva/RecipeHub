09691ad45ea879f0d75e12e92f8dc192c31aa2df
Diana Kovacheva <dmarinova@outlook.com>
2023-08-12 02:28:04 +0200

    Adjusts the comment recipe form

--- src/app/recipe/comment-recipe/comment-recipe.component.ts
+++ src/app/recipe/comment-recipe/comment-recipe.component.ts
@@ -1,30 +1,24 @@
 import { Component, Input } from "@angular/core";
-import { CommonModule, IMAGE_CONFIG } from "@angular/common";
+import { CommonModule } from "@angular/common";
 import { MatButtonModule } from "@angular/material/button";
-import { ActivatedRoute, Router, RouterModule } from "@angular/router";
+import { RouterModule } from "@angular/router";
 import { RecipeService } from "../recipe.service";
 import { SnackBarService } from "../../shared/snack-bar-notification/snack-bar.service";
 import { Recipe } from "src/app/models/Recipe";
 import { Comment } from "src/app/models/Comment";
 import { DeleteRecipeComponent } from "../delete-recipe/delete-recipe.component";
-import {
-  MatDialogRef,
-  MatDialogModule,
-  MAT_DIALOG_DATA,
-} from "@angular/material/dialog";
+import { MatDialogModule } from "@angular/material/dialog";
 import {
   FormControl,
   Validators,
   FormsModule,
   ReactiveFormsModule,
-  FormGroup,
 } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { UserService } from "src/app/user/user.service";
 import { UserId } from "src/app/models/UserId";
 import { RecipeDetailsComponent } from "../recipe-details/recipe-details.component";
-import "../../shared/ng-form.extensions";
 
 export interface DialogData {
   recipe: Recipe;
@@ -61,17 +55,15 @@ export class CommentRecipeComponent {
     private recipeDetailsComponent: RecipeDetailsComponent // @Inject(MAT_DIALOG_DATA) public data: DialogData,
   ) {}
 
-  commentForm = new FormGroup({
-    commentContent: new FormControl("", [
-      Validators.required,
-      Validators.minLength(2),
-      Validators.maxLength(500),
-    ]),
-  });
+  commentContent = new FormControl("", [
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(500),
+  ]);
 
   // Check Error Functions
   getErrorMessageCommentContent() {
-    const commentInput = this.commentForm.get("commentContent");
+    const commentInput = this.commentContent;
 
     if (commentInput!.hasError("required")) {
       return "You must enter a value.";
@@ -90,15 +82,13 @@ export class CommentRecipeComponent {
   //   this.dialogRef.close();
   // }
 
-  onConfirm(): void {
+  onConfirm($event: { preventDefault: () => void }): void {
+    $event.preventDefault();
     this.recipeService
-      .commentRecipe(
-        this.recipeId!,
-        this.commentForm.value.commentContent ?? ""
-      )
+      .commentRecipe(this.recipeId!, this.commentContent.value ?? "")
       .subscribe({
         next: () => {
-          // this.commentForm.resetValidation();
+          this.markUntouched();
           this.recipeDetailsComponent.getRecipeById();
           this.recipeDetailsComponent.getRecipeComments();
           this.userService.getProfile();
@@ -106,5 +96,10 @@ export class CommentRecipeComponent {
         },
         error: (err) => this.snackBar.notifyError(err.error.message),
       });
+  }
+
+  markUntouched() {
+    this.commentContent.reset();
+    this.commentContent.markAsUntouched();
   }
 }
